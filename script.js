ef18f9d0643507c0a3f784b4df9ef51ab433b1da
Miura Satoshi <107822229+MiuraSatoshi3104@users.noreply.github.com>
2023-03-20 19:22:52 +0900

    Update script.js
    Signed-off-by: Miura Satoshi <107822229+MiuraSatoshi3104@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const smartphoneScreen = document.getElementById("smartphoneScreen");
-    const storedImage = localStorage.getItem("uploadedImage");
-    if (storedImage) {
-      smartphoneScreen.style.backgroundImage = `url(${storedImage})`;
-      smartphoneScreen.style.backgroundSize = "cover";
-      smartphoneScreen.style.backgroundPosition = "center";
-    }
-  });
-  
-  function uploadImage() {
-    const input = document.getElementById("file-upload");
-    if (input.files && input.files[0]) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        localStorage.setItem("uploadedImage", e.target.result);
-        window.location.href = "index.html";
-      };
-      reader.readAsDataURL(input.files[0]);
-    }
+  const smartphoneScreen = document.getElementById("smartphoneScreen");
+  const storedImage = localStorage.getItem("uploadedImage");
+  if (storedImage) {
+    const imageContainer = document.createElement("div");
+    imageContainer.className = "image-container";
+    const img = document.createElement("img");
+    img.src = storedImage;
+    imageContainer.appendChild(img);
+    smartphoneScreen.appendChild(imageContainer);
   }
-  
+});
+
+function uploadImage() {
+  const input = document.getElementById("file-upload");
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      localStorage.setItem("uploadedImage", e.target.result);
+      window.location.href = "index.html";
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
