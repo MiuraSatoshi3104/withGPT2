9741de3eb256dc382362955b64c87fa3824e9a67
Miura Satoshi <107822229+MiuraSatoshi3104@users.noreply.github.com>
2023-03-20 20:04:25 +0900

    Update script.js
    Signed-off-by: Miura Satoshi <107822229+MiuraSatoshi3104@users.noreply.github.com>

--- script.js
+++ script.js
@@ -6,8 +6,30 @@ document.addEventListener("DOMContentLoaded", () => {
     img.src = storedImage;
     img.classList.add("uploaded-image");
     smartphoneScreen.appendChild(img);
+  } else {
+    localStorage.removeItem("uploadedImage");
   }
 });
+
+let currentImage = 0;
+const images = [];
+const imageCount = 3;
+
+for (let i = 1; i <= imageCount; i++) {
+  images.push(`image${i}.jpg`);
+}
+
+function loadImage() {
+  const image = images[currentImage];
+  const img = document.createElement("img");
+  img.src = image;
+  img.classList.add("uploaded-image");
+  const smartphoneScreen = document.getElementById("smartphoneScreen");
+  smartphoneScreen.innerHTML = "";
+  smartphoneScreen.appendChild(img);
+}
+
+loadImage();
 
 function uploadImage() {
   const input = document.getElementById("file-upload");
@@ -20,3 +42,13 @@ function uploadImage() {
     reader.readAsDataURL(input.files[0]);
   }
 }
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowRight") {
+    currentImage = (currentImage + 1) % images.length;
+    loadImage();
+  } else if (event.key === "ArrowLeft") {
+    currentImage = (currentImage - 1 + images.length) % images.length;
+    loadImage();
+  }
+});
